94e692b6013d05a15fc5251b20f54f315bfe9a9a
Dhanashekar7131 <167563874+Dhanashekar7131@users.noreply.github.com>
2024-07-13 15:56:00 +0530

    Update banking.tf

--- banking.tf
+++ banking.tf
@@ -5,8 +5,6 @@ terraform {
       version = "~> 4.0"
     }
   }
-  
-  required_version = ">= 1.1.0"
 }
 
 provider "aws" {
@@ -14,128 +12,137 @@ provider "aws" {
 }
 
 # VPC
-resource "aws_vpc" "main" {
+resource "aws_vpc" "proj-vpc" {
   cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "main_vpc"
-  }
 }
 
 # Internet Gateway
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
+resource "aws_internet_gateway" "proj-ig" {
+  vpc_id = aws_vpc.proj-vpc.id
 
   tags = {
-    Name = "main_igw"
+    Name = "gateway1"
   }
 }
 
 # Subnet
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
+resource "aws_subnet" "proj-subnet" {
+  vpc_id            = aws_vpc.proj-vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "main_subnet"
+    Name = "subnet1"
   }
 }
 
 # Route Table
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
+resource "aws_route_table" "proj-rt" {
+  vpc_id = aws_vpc.proj-vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
+    gateway_id = aws_internet_gateway.proj-ig.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id      = aws_internet_gateway.proj-ig.id
   }
 
   tags = {
-    Name = "main_route_table"
+    Name = "rt1"
   }
 }
 
 # Route Table Association
-resource "aws_route_table_association" "main" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.main.id
+resource "aws_route_table_association" "proj-rt-sub-assoc" {
+  subnet_id      = aws_subnet.proj-subnet.id
+  route_table_id = aws_route_table.proj-rt.id
 }
 
 # Security Group
-resource "aws_security_group" "allow_all" {
-  vpc_id = aws_vpc.main.id
+resource "aws_security_group" "proj-sg" {
+  vpc_id = aws_vpc.proj-vpc.id
 
   ingress {
+    description = "Allow SSH traffic from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTP traffic from anywhere"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS traffic from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow all inbound traffic within the security group"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    self        = true
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description      = "Allow all outbound traffic to anywhere"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
-    Name = "allow_all_sg"
+    Name = "proj-sg1"
   }
 }
 
 # Network Interface
-resource "aws_network_interface" "main" {
-  subnet_id       = aws_subnet.main.id
-  private_ips     = ["10.0.1.50"]
-  security_groups = [aws_security_group.allow_all.id]
+resource "aws_network_interface" "proj-ni" {
+  subnet_id       = aws_subnet.proj-subnet.id
+  private_ips     = ["10.0.1.10"]
+  security_groups = [aws_security_group.proj-sg.id]
 
   tags = {
-    Name = "main_network_interface"
+    Name = "proj_network_interface"
   }
 }
 
 # Elastic IP
-resource "aws_eip" "main" {
+resource "aws_eip" "proj-eip" {
   vpc = true
 
   # Attach the EIP to the network interface
-  network_interface = aws_network_interface.main.id
+  network_interface = aws_network_interface.proj-ni.id
+  associate_with_private_ip = "10.0.1.10"
 }
 
 # EC2 Instance
-resource "aws_instance" "prod_test_server" {
-  ami             = "ami-0a313d6098716f372" # Ubuntu 22.04 AMI ID for us-east-1
-  instance_type   = "t2.micro"
-  key_name        = "keypair" # Replace with your key pair name
+resource "aws_instance" "prod_New_test_server" {
+  ami           = "ami-0a313d6098716f372"  # Replace with your AMI ID
+  instance_type = "t2.micro"
+  key_name      = "keypair"                # Replace with your key pair name
 
   network_interface {
-    network_interface_id = aws_network_interface.main.id
+    network_interface_id = aws_network_interface.proj-ni.id
     device_index         = 0
   }
 
   tags = {
-    Name = "Prod_test_server"
+    Name = "Prod_New_test_server"
   }
-}
-
-output "instance_id" {
-  value = aws_instance.prod_test_server.id
-}
-
-output "instance_public_ip" {
-  value = aws_eip.main.public_ip
-}
-
-output "vpc_id" {
-  value = aws_vpc.main.id
-}
-
-output "subnet_id" {
-  value = aws_subnet.main.id
-}
-
-output "security_group_id" {
-  value = aws_security_group.allow_all.id
 }
