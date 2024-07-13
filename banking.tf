cbc672ab9486a335e79c9bbfdcc5784c877ed775
Dhanashekar7131 <167563874+Dhanashekar7131@users.noreply.github.com>
2024-07-13 13:59:18 +0530

    Create banking.tf

---
+++ banking.tf
@@ -0,0 +1,141 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main_vpc"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "main_igw"
+  }
+}
+
+# Subnet
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "main_subnet"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "main_route_table"
+  }
+}
+
+# Route Table Association
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+
+# Security Group
+resource "aws_security_group" "allow_all" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_all_sg"
+  }
+}
+
+# Network Interface
+resource "aws_network_interface" "main" {
+  subnet_id       = aws_subnet.main.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_all.id]
+
+  tags = {
+    Name = "main_network_interface"
+  }
+}
+
+# Elastic IP
+resource "aws_eip" "main" {
+  vpc = true
+
+  # Attach the EIP to the network interface
+  network_interface = aws_network_interface.main.id
+}
+
+# EC2 Instance
+resource "aws_instance" "prod_test_server" {
+  ami             = "ami-0a313d6098716f372" # Ubuntu 22.04 AMI ID for us-east-1
+  instance_type   = "t2.micro"
+  key_name        = "keypair" # Replace with your key pair name
+
+  network_interface {
+    network_interface_id = aws_network_interface.main.id
+    device_index         = 0
+  }
+
+  tags = {
+    Name = "Prod_test_server"
+  }
+}
+
+output "instance_id" {
+  value = aws_instance.prod_test_server.id
+}
+
+output "instance_public_ip" {
+  value = aws_eip.main.public_ip
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.main.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.allow_all.id
+}
